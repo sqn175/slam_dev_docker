bf9f022dddb49135d5e71fefb8ebe8ade24b2828
sqn175 <shi-q15@mails.tsinghua.edu.cn>
2021-11-11 11:30:56 +0800

    modify 3rd parties version

--- installers/install_3rdparties_planar_slam.sh
+++ installers/install_3rdparties_planar_slam.sh
@@ -6,17 +6,17 @@ export ARCHIVE_DIR="/tmp/archive"
 CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
 # 1) Install some dependencies via apt
-# apt-get -y update && \
-#     apt-get -y install --no-install-recommends \
-#     
+apt-get -y update && \
+    apt-get -y install --no-install-recommends \
+    libcanberra-gtk-module
 
 # 2) Install dependencies via buiding from source
 # Eigen3 and OpenCV should be installed first as they may be 
 # the dependencies of other libs
-bash ${CURR_DIR}/install_eigen3.sh      3.1.0
+bash ${CURR_DIR}/install_eigen3.sh      3.3.0
 bash ${CURR_DIR}/install_opencv.sh      3.4.1
 bash ${CURR_DIR}/install_pangolin.sh
-bash ${CURR_DIR}/install_pcl.sh         1.9.0 gpu
+bash ${CURR_DIR}/install_pcl.sh         1.10.1
 
 # Clean up cache to reduce layer size.
 apt-get clean && \
