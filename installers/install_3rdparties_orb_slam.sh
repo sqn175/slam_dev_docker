b1bc7e3bebd9902ded3296b2ab3f0e232354e03a
sqn175 <shi-q15@mails.tsinghua.edu.cn>
2021-11-12 17:00:34 +0800

    support ORB SLAM2&3

---
+++ installers/install_3rdparties_orb_slam.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+export ARCHIVE_DIR="/tmp/archive"
+CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+
+# 1) Install some dependencies via apt
+# apt-get -y update && \
+#     apt-get -y install --no-install-recommends \
+#     
+
+# 2) Install dependencies via buiding from source
+# Eigen3 and OpenCV should be installed first as they may be 
+# the dependencies of other libs
+bash ${CURR_DIR}/install_eigen3.sh      3.3.4
+bash ${CURR_DIR}/install_opencv.sh      3.2.0
+bash ${CURR_DIR}/install_pangolin.sh
+
+# Clean up cache to reduce layer size.
+apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
